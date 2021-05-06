e17f4b561bd23441db540c1ca3f37bceab119a0e
AndyZe <zelenak@picknik.ai>
2021-05-06 10:27:40 -0500

    Add MoveIt2 logo and waypoints yaml

--- src/parse_image_waypoints.cpp
+++ src/parse_image_waypoints.cpp
@@ -1,6 +1,5 @@
 #include <Eigen/Core>
 #include <Eigen/Geometry>
-#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <iostream>
 #include <light_painting_demo/parse_image_waypoints.h>
 
@@ -32,7 +31,7 @@ bool ParseImageWaypoints::loadWaypointsFromFile(const std::string& yaml_file)
   return true;
 }
 
-bool ParseImageWaypoints::transformPixelCoordinatesToRobotPose()
+std::vector<geometry_msgs::msg::PoseStamped> ParseImageWaypoints::transformPixelCoordinatesToRobotPose()
 {
   // (0,0) is the upper-left pixel coordinate in the image
   // (500,500) is the center pixel of the image
@@ -41,18 +40,39 @@ bool ParseImageWaypoints::transformPixelCoordinatesToRobotPose()
 
   // So, to transform from pixel coordinates to robot coordinates, do...
   // 1. scale by 0.001
-  // 2. rotate 180* about X
+  // 2. translate by (-0.5, -0.5)
+  // 3. do the inverse of (rotate (90* about X) then (90* about Z))
 
+  std::vector<geometry_msgs::msg::PoseStamped> target_poses;
   geometry_msgs::msg::PoseStamped target_pose;
   target_pose.header.frame_id = WAYPOINT_FRAME_ID;
 
   Eigen::Isometry3d transform(Eigen::Isometry3d::Identity());
 
+  Eigen::Quaterniond rot_90_about_x(0.7071, 0.7071, 0, 0);
+  Eigen::Quaterniond rot_90_about_z(0.7071, 0, 0, 0.7071);
+  Eigen::Quaterniond net_rotation_from_pixel_to_world = (rot_90_about_x * rot_90_about_z).inverse();
+
   for (size_t waypoint_idx = 0; waypoint_idx < pixel_waypoints_x_y_.size(); ++waypoint_idx)
   {
     transform = Eigen::Isometry3d::Identity();
+    transform.translation().x() = pixel_waypoints_x_y_[waypoint_idx].first * PIXEL_TO_WORLD_SCALE - 0.5;
+    transform.translation().y() = pixel_waypoints_x_y_[waypoint_idx].second * PIXEL_TO_WORLD_SCALE - 1.0;
+
+    transform = net_rotation_from_pixel_to_world * transform;
+
+    target_pose.pose.position.x = 0;
+    target_pose.pose.position.y = transform.translation().y();
+    target_pose.pose.position.z = transform.translation().z();
+    target_poses.push_back(target_pose);
+
+    std::cout << "Waypoint (x,y,z) in frame " << WAYPOINT_FRAME_ID << ":  "
+              << 0
+              << "  " << target_pose.pose.position.y
+              << "  " << target_pose.pose.position.z
+              << std::endl;
   }
 
-  return true;
+  return target_poses;
 }
 }  // namespace light_painting_demo
