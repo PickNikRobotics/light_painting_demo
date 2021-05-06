47f1f6fc5cc68c6fc4433896c9452445bb7f6970
AndyZe <zelenak@picknik.ai>
2021-05-06 12:58:50 -0500

    Define moveit controllers

--- launch/ur_moveit_cpp.launch.py
+++ launch/ur_moveit_cpp.launch.py
@@ -261,7 +261,7 @@ def generate_launch_description():
     ompl_planning_pipeline_config["move_group"].update(ompl_planning_yaml)
 
     # Trajectory Execution Configuration
-    controllers_yaml = load_yaml("ur_moveit_config", "config/controllers.yaml")
+    controllers_yaml = load_yaml("light_painting_demo", "config/moveit_controllers.yaml")
     moveit_controllers = {
         "moveit_simple_controller_manager": controllers_yaml,
         "moveit_controller_manager": "moveit_simple_controller_manager/MoveItSimpleControllerManager",
